b430c03e45128319af9348441f1187d0959c202d
fatiihvarol <fatihvrl38@gmail.com>
2024-10-09 13:59:37 +0300

    routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { LoginComponent } from './login/login.component';
 import { AddActivityComponent } from './add-activity/add-activity.component';  // Example for another route
 import { MainComponent } from './main/main.component';  // Example for another route
 import { NgModule } from '@angular/core';
+import { DetailComponent } from './detail/detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'add-activity', component: AddActivityComponent },  
   { path: 'main', component: MainComponent },
+  { path: 'todo-detail/:id', component: DetailComponent },
+
 
 
 ];
